ca350a5887a9ea104cdf8991be1142cca2c5921b
Natalya Myunster <natalyamyunster@yahoo.com>
2020-10-02 02:24:56 +0200

    Updates

---
+++ precache-manifest.7542e2cffc5b0c3dc0571b6b249d4cf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39e186429d78916f7d9628c68c353851",
+    "url": "/natalina27.github.io/weather-app-front/index.html"
+  },
+  {
+    "revision": "b45062bba059bf22658b",
+    "url": "/natalina27.github.io/weather-app-front/static/css/main.76958ddc.chunk.css"
+  },
+  {
+    "revision": "b18e98f3ed3d222ae045",
+    "url": "/natalina27.github.io/weather-app-front/static/js/2.a98956ec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/natalina27.github.io/weather-app-front/static/js/2.a98956ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b45062bba059bf22658b",
+    "url": "/natalina27.github.io/weather-app-front/static/js/main.7edd0f3e.chunk.js"
+  },
+  {
+    "revision": "bf2d2a7f8e0b40a041dc",
+    "url": "/natalina27.github.io/weather-app-front/static/js/runtime-main.1f9cefe0.js"
+  }
+]);
